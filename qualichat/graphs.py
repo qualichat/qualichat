65472ad1ef21b14c8a2f14e9adfa74bd58874b0c
Caio Alexandre <contato.kyomi@gmail.com>
2021-06-09 17:09:12 -0300

    Rewrite graphs module

--- qualichat/graphs.py
+++ qualichat/graphs.py
@@ -22,19 +22,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-import datetime
-from typing import Union, List
-from pathlib import Path
+from typing import Union, List, Callable, DefaultDict
 from collections import defaultdict
-from itertools import cycle, islice
+from pathlib import Path
 
 import matplotlib
+from pandas import DataFrame
 from matplotlib import pyplot as plot
 from matplotlib import font_manager
-from pandas import DataFrame
 
 from .chat import Qualichat
-from .enums import Period, SubPeriod
+from .models import Message
 
 
 # Add ``Inter`` font.
@@ -46,89 +44,119 @@ for font in font_manager.findSystemFonts(str(fonts)):
 matplotlib.rcParams['font.family'] = 'Inter'
 
 
-DEFAULT_COLORS     = ['#08bcac', '#38444c']
-SECONDARY_COLORS   = ['#f2c80f', '#fd625e', '#8ad4eb', '#b887ad']
-TERTIARY_COLORS    = ['#796408', '#374649', '#808080', '#a66999', '#fe9666', '#fae99f', '#f5d33f']
-DEFAULT_LINE_COLOR = '#ff645c'
+BARS_COLORS = {
+    2: ['#08bcac', '#38444c'],
+    4: ['#f2c80f', '#fd625e', '#8ad4eb', '#b887ad']
+}
+
+LINE_COLORS = {
+    0: '#ff645c',
+    2: '#ff645c',
+    4: '#000000'
+}
+
+
+def generate_graph(
+        *,
+        bars: List[str] = [],
+        lines: List[str] = [],
+        title: str = None
+    ):
+    def decorator(method: Callable[[], DataFrame]):
+        def wrapped(*args, **kwargs):
+            fig, ax = plot.subplots()
+            dataframe = method(*args, **kwargs)
+
+            ax.set_title(title)
+
+            if bars:
+                color = BARS_COLORS[len(bars)]
+
+                bars_dataframe = dataframe.filter(bars)
+                ax2 = bars_dataframe.plot.bar(ax=ax, rot=0, color=color)
+                ax2.legend(loc='upper right')
+
+            if lines:
+                color = LINE_COLORS[len(bars)]
+
+                lines_dataframe = dataframe.filter(lines)
+                ax3 = lines_dataframe.plot(ax=ax, secondary_y=True, color=color)
+
+            ax.grid(axis='y', linestyle='solid')
+            plot.show()
+
+        return wrapped
+    return decorator
 
 
 class GraphGenerator:
-    '''This class provides the ability to generate graphs.
+    """This class provides the ability to generate graphs.
     
     Attributes
     ----------
     chats: List[:class:`.Qualichat`]
         The list of chats to be analyzed.
-    '''
-    __slots__ = ('chats',)
+    """
 
+    __slots__ = ('chats',)
+    
     def __init__(self, chats: Union[Qualichat, List[Qualichat]]):
         if isinstance(chats, Qualichat):
             chats = [chats]
 
         self.chats = chats
 
-    def by_messages_per_month(self):
-        '''Shows the number of messages per month. Shows the number of liquid characters
-        and the number of text characters.
-        '''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by Month')
-
+    @generate_graph(
+        bars=['QTD_Liquido', 'QTD_Texto'],
+        lines=['QTD_Mensagens'],
+        title='Amount by Month'
+    )
+    def messages_per_month(self):
+        """Shows the number of messages per month. Shows the number of
+        liquid characters and the number of text characters.
+        """
         chat = self.chats[0]
-        data = defaultdict(list)
+        data: DefaultDict[str, List[Message]] = defaultdict(list)
 
         columns = ['QTD_Liquido', 'QTD_Texto', 'QTD_Mensagens']
         rows = []
 
         for message in chat.messages:
             index = message.created_at.replace(day=1, hour=0, minute=0, second=0)
-            strftime = index.strftime('%B %Y')
-            data[strftime].append(message)
+            data[index.strftime('%B %Y')].append(message)
 
         for values in data.values():
-            liquid = 0
-            text = 0
+            net_content = 0
+            pure_content = 0
             messages = 0
 
             for message in values:
-                liquid += len(message.liquid)
-                text += len(message.pure_text)
+                net_content += len(message['Qntd_Caract_Liquido'])
+                pure_content += len(message['Qntd_Caract_Texto'])
                 messages += 1
 
-            rows.append([liquid, text, messages])
+            rows.append([net_content, pure_content, messages])
 
-        dataframe = DataFrame(rows, index=data.keys(), columns=columns)
+        return DataFrame(rows, index=data.keys(), columns=columns)
 
-        bars = dataframe.drop(columns='QTD_Mensagens')
-        ax2 = bars.plot.bar(ax=ax, rot=0, color=DEFAULT_COLORS)
-
-        messages = dataframe['QTD_Mensagens']
-        ax3 = messages.plot(ax=ax.twiny(), secondary_y=True, color=DEFAULT_LINE_COLOR)
-        
-        ax.grid(axis='y', linestyle='solid')
-        ax2.legend(loc='upper right')
-        ax3.legend(loc='upper left')
-
-        plot.show()
-
-    def by_message_aspects(self):
-        '''Shows the messages aspects per month. Shows the number of laughs, marks, emojis
-        and numbers characters sent per month.
-        '''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by Month')
-
+    @generate_graph(
+        bars=['QTD_Riso', 'QTD_Pontuacao', 'QTD_Emoji', 'QTD_Numeros'],
+        lines=['QTD_Mensagens'],
+        title='Amount by Month'
+    )
+    def message_aspects(self):
+        """Shows the messages aspects per month. Shows the number of
+        laughs, marks, emojis and numbers characters sent per month.
+        """
         chat = self.chats[0]
-        data = defaultdict(list)
+        data: DefaultDict[str, List[Message]] = defaultdict(list)
 
         columns = ['QTD_Riso', 'QTD_Pontuacao', 'QTD_Emoji', 'QTD_Numeros', 'QTD_Mensagens']
         rows = []
 
         for message in chat.messages:
             index = message.created_at.replace(day=1, hour=0, minute=0, second=0)
-            strftime = index.strftime('%B %Y')            
-            data[strftime].append(message)
+            data[index.strftime('%B %Y')].append(message)
 
         for values in data.values():
             laughs = 0
@@ -138,220 +166,12 @@ class GraphGenerator:
             messages = 0
 
             for message in values:
-                laughs += len(message.laughs)
-                marks += len(message.question_marks) + len(message.exclamation_marks)
-                emojis += len(message.emojis)
-                numbers += len(message.numbers)
+                laughs += len(''.join(message['Qntd_Caract_Risos']))
+                marks += len(''.join(message['Qntd_Caract_Pontuacao']))
+                emojis += len(''.join(message['Qntd_Caract_Emoji']))
+                numbers += len(''.join(message['Qntd_Caract_Numeros']))
                 messages += 1
                 
             rows.append([laughs, marks, emojis, numbers, messages])
 
-        dataframe = DataFrame(rows, index=data.keys(), columns=columns)
-
-        bars = dataframe.drop(columns='QTD_Mensagens')
-        ax2 = bars.plot.bar(ax=ax, rot=0, color=SECONDARY_COLORS)
-
-        messages = dataframe['QTD_Mensagens']
-        ax3 = messages.plot(ax=ax.twiny(), secondary_y=True, color='#000000')
-
-        ax.grid(axis='y', linestyle='solid')
-        ax2.legend(loc='upper right')
-        ax3.legend(loc='upper left')
-
-        plot.show()
-
-    def by_weekday(self):
-        '''Shows which weekdays are more active.'''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by Month')
-
-        chat = self.chats[0]
-        data = defaultdict(list)
-
-        columns = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'QTD_Liquido']
-        rows = []
-
-        for message in chat.messages:
-            index = message.created_at.replace(day=1, hour=0, minute=0, second=0)
-            strftime = index.strftime('%B %Y')
-            data[strftime].append(message)
-
-        for values in data.values():
-            liquid = 0
-            weekdays = {
-                'Sunday': 0,
-                'Monday': 0,
-                'Tuesday': 0,
-                'Wednesday': 0,
-                'Thursday': 0,
-                'Friday': 0,
-                'Saturday': 0
-            }
-
-            for message in values:
-                liquid += 1
-                weekdays[message.weekday] += 1
-
-            rows.append([*weekdays.values(), liquid])
-
-        dataframe = DataFrame(rows, index=data.keys(), columns=columns)
-
-        bars = dataframe.drop(columns='QTD_Liquido')
-        ax2 = bars.plot.bar(ax=ax, rot=0, color=TERTIARY_COLORS)
-
-        messages = dataframe['QTD_Liquido']
-        ax3 = messages.plot(ax=ax.twiny(), secondary_y=True, color='#01b8aa')
-
-        ax.grid(axis='y', linestyle='solid')
-        ax2.legend(loc='upper right')
-        ax3.legend(loc='upper left')
-
-        plot.show()
-
-    def by_message_period(self):
-        '''Shows which periods are more active.'''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by Month')
-
-        chat = self.chats[0]
-        data = defaultdict(list)
-
-        columns = [c.value for c in Period]
-        rows = []
-
-        for message in chat.messages:
-            index = message.created_at.replace(day=1, hour=0, minute=0, second=0)
-            strftime = index.strftime('%B %Y')
-            data[strftime].append(message)
-
-        for values in data.values():
-            periods = {c.value: 0 for c in Period}
-
-            for message in values:
-                periods[message.period.value] += 1
-
-            rows.append(list(periods.values()))
-
-        dataframe = DataFrame(rows, index=data.keys(), columns=columns)
-        dataframe.plot(ax=ax, rot=0, color=SECONDARY_COLORS)
-
-        plot.show()
-
-    def by_message_sub_period(self):
-        '''Shows which sub-periods are more active.'''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by Month')
-
-        chat = self.chats[0]
-        data = defaultdict(list)
-
-        columns = [c.value for c in SubPeriod]
-        rows = []
-
-        for message in chat.messages:
-            index = message.created_at.replace(day=1, hour=0, minute=0, second=0)
-            strftime = index.strftime('%B %Y')
-            data[strftime].append(message)
-
-        for values in data.values():
-            sub_periods = {c.value: 0 for c in SubPeriod}
-
-            for message in values:
-                sub_periods[message.sub_period.value] += 1
-
-            rows.append(list(sub_periods.values()))
-
-        dataframe = DataFrame(rows, index=data.keys(), columns=columns)
-        dataframe.plot(ax=ax, rot=0, color=SECONDARY_COLORS)
-
-        plot.show()
-
-    def by_users_activity(self):
-        '''Shows which users are more active in the chat.'''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by User')
-
-        chat = self.chats[0]
-        data = {}
-
-        columns = ['QTD_Liquido', 'QTD_Texto', 'QTD_Mensagens']
-        rows = []
-
-        for actor in chat.actors:
-            data[actor.display_name] = actor.messages
-
-        for values in data.values():
-            liquid = 0
-            pure_text = 0
-            
-            for message in values:
-                liquid += len(message.liquid)
-                pure_text += len(message.pure_text)
-
-            rows.append([liquid, pure_text, len(values)])
-
-        temp = DataFrame(rows, index=data.keys(), columns=columns)[:10]
-        dataframe = temp.sort_values(columns, ascending=False)
-
-        bars = dataframe.drop(columns='QTD_Mensagens')
-        ax2 = bars.plot.bar(ax=ax, rot=0, color=DEFAULT_COLORS)
-
-        messages = dataframe['QTD_Mensagens']
-        ax3 = messages.plot(ax=ax.twiny(), secondary_y=True, color=DEFAULT_LINE_COLOR)
-
-        ax.grid(axis='y', linestyle='solid')
-        ax2.legend(loc='upper right')
-        ax3.legend(loc='upper left')
-
-        plot.show()
-
-    def by_users_messages_aspects(self):
-        '''Shows the messages aspects per user. Shows the number of laughs, marks, 
-        numbers characters sent per user.'''
-        fig, ax = plot.subplots()
-        ax.set_title('Amount by User')
-
-        chat = self.chats[0]
-        data = {}
-
-        columns = ['QTD_Numero', 'QTD_Pontuacao', 'QTD_Riso', 'QTD_Mensagens']
-        rows = []
-
-        for actor in chat.actors:
-            data[actor.display_name] = actor.messages
-
-        for values in data.values():
-            laughs = 0
-            marks = 0
-            numbers = 0
-
-            for message in values:
-                for number in message.numbers:
-                    numbers += len(number)
-
-                for exclamation_mark in message.exclamation_marks:
-                    marks += len(exclamation_mark)
-
-                for question_mark in message.question_marks:
-                    marks += len(question_mark)
-                
-                for laugh in message.laughs:
-                    laughs += len(laugh)
-
-            rows.append([numbers, marks, laughs, len(values)])
-
-        temp = DataFrame(rows, index=data.keys(), columns=columns)[:10]
-        dataframe = temp.sort_values(columns, ascending=False)
-
-        bars = dataframe.drop(columns='QTD_Mensagens')
-        ax2 = bars.plot.bar(ax=ax, rot=0, color=SECONDARY_COLORS)
-
-        messages = dataframe['QTD_Mensagens']
-        ax3 = messages.plot(ax=ax.twiny(), secondary_y=True, color='#000000')
-
-        ax.grid(axis='y', linestyle='solid')
-        ax2.legend(loc='upper right')
-        ax3.legend(loc='upper left')
-
-        plot.show()
-            
+        return DataFrame(rows, index=data.keys(), columns=columns)
