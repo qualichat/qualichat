7fae826b57bbdff1cf1456431018b19cb3a34768
Caio Alexandre <contato.kyomi@gmail.com>
2021-06-16 17:45:50 -0300

    Add `GraphGenerator.users_messages_marks()` method

--- qualichat/graphs.py
+++ qualichat/graphs.py
@@ -338,6 +338,38 @@ class GraphGenerator:
         return dataframe[:10].sort_values(columns, ascending=False)
 
     @generate_graph(
+        bars=['Qty_char_!', 'Qty_char_?'],
+        lines=['Qty_messages'],
+        title='Amount by Users'
+    )
+    def users_messages_marks(self):
+        """hows the amount of exclamation points and question marks
+        sent by user.
+        """
+        chat = self.chats[0]
+        data = {}
+
+        columns = ['Qty_char_!', 'Qty_char_?', 'Qty_messages']
+        indexes = [actor.display_name for actor in chat.actors]
+
+        rows = []
+
+        for actor in chat.actors:
+            exclamation_marks = 0
+            question_marks = 0
+            messages = 0
+
+            for message in actor.messages:
+                exclamation_marks += get_length(message['Qty_char_!'])
+                question_marks += get_length(message['Qty_char_?'])
+                messages += 1
+
+            rows.append([exclamation_marks, question_marks, messages])            
+    
+        dataframe = DataFrame(rows, index=indexes, columns=columns)
+        return dataframe[:10].sort_values(columns, ascending=False)
+
+    @generate_graph(
         bars=PERIODS,
         lines=['Qty_messages'],
         title='Amount by Month'
